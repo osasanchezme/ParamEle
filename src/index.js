2188a75a10993a5515033f105210840e7203eae4
Oscar Sanchez <osasanchezme@unal.edu.co>
2022-07-30 17:46:01 -0500

    New components library and basic ui structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+class NavBar extends React.Component {
+  render() {
+    return <div className="nav-bar"></div>;
+  }
+}
+class Renderer extends React.Component {
+  render() {
+    return <div className="renderer-container"></div>;
+  }
+}
+class VisualEditor extends React.Component {
+  render() {
+    return <div className="editor-container"></div>;
+  }
+}
+
+class GlobalControls extends React.Component {
+  render() {
+    return (
+      <div className="global-controls">
+        <div className="global-controls-hidden"></div>
+        <div className="global-controls-container"></div>
+      </div>
+    );
+  }
+}
 
 class ParamEle extends React.Component {
-    render() {
-        return (
-            <div className="app-cont">
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <ChakraProvider>
+        <div className="app-cont">
+          <NavBar></NavBar>
+          <GlobalControls></GlobalControls>
+          <VisualEditor></VisualEditor>
+          <Renderer></Renderer>
+        </div>
+      </ChakraProvider>
+    );
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
