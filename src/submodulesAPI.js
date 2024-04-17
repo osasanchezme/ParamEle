a6f12fb1632b43923efdee65642be2f05bc53540
Oscar Sanchez <osasanchezme@unal.edu.co>
2024-04-17 16:28:46 -0500

    Update submodule paramele-parsers and fix packing issue

--- src/submodulesAPI.js
+++ src/submodulesAPI.js
@@ -1,9 +1,9 @@
-import generateInputFile from "./submodules/paramele-parsers/structural/csi/generateInputFile";
+import generateInputFileESM from "./submodules/paramele-parsers/structural/csi/generateInputFileESM";
 
 
 const parsers = {
   csi: {
-    getSAP2000Model: (s3d_model) => generateInputFile(s3d_model, "SAP2000", "25.1.0"),
+    getSAP2000Model: (s3d_model) => generateInputFileESM(s3d_model, "SAP2000", "25.1.0"),
   },
 };
 export { parsers };
