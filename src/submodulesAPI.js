d83b25e3ee18d33b0f5b03f36b87e58e9d5bf127
Oscar Sanchez <osasanchezme@unal.edu.co>
2024-04-17 17:48:16 -0500

    Disable the SAP converter temporarily

--- src/submodulesAPI.js
+++ src/submodulesAPI.js
@@ -1,9 +1,10 @@
-import generateInputFileESM from "./submodules/paramele-parsers/structural/csi/generateInputFileESM";
+// import generateInputFileESM from "./submodules/paramele-parsers/structural/csi/generateInputFileESM.js";
 
 
 const parsers = {
   csi: {
-    getSAP2000Model: (s3d_model) => generateInputFileESM(s3d_model, "SAP2000", "25.1.0"),
+    // getSAP2000Model: (s3d_model) => generateInputFileESM(s3d_model, "SAP2000", "25.1.0"),
+    getSAP2000Model: (s3d_model) => 'Feature temporarily disabled',
   },
 };
 export { parsers };
